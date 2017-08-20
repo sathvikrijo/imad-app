5a914fb9a816dab002284e8efb8ed0908bcd8bcd
sathvikrijo <30945817+sathvikrijo@users.noreply.github.com>
2017-08-20 23:01:40 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -5,10 +5,10 @@ var path = require('path');
 var app = express();
 app.use(morgan('combined'));
 
-var articles: {
-    var articleOne= {
+var articles= {
+    articleOne :{
     title: 'ARTICLE 1',
-    head1 : 'This is Article 1 Deployed By Sathvik',
+    head : 'This is Article 1 Deployed By Sathvik',
     content : `
                 <p>
                     Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Aenean commodo ligula eget dolor. Aenean massa. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Donec quam felis, ultricies nec, pellentesque eu, pretium quis, sem. Nulla consequat massa quis enim. Donec pede justo, fringilla vel, aliquet nec, vulputate eget, arcu. In enim justo, rhoncus ut, imperdiet a, venenatis vitae, justo. Nullam dictum felis eu pede mollis pretium. Integer tincidunt. Cras dapibus. Vivamus elementum semper nisi. Aenean vulputate eleifend tellus. Aenean leo ligula, porttitor eu, consequat vitae, eleifend ac, enim. Aliquam lorem ante, dapibus in, viverra quis, feugiat a, tellus. Phasellus viverra nulla ut metus varius laoreet. Quisque rutrum. Aenean imperdiet. Etiam ultricies nisi vel augue. Curabitur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
@@ -19,14 +19,22 @@ var articles: {
                 </p>`
     
 },
-    var articlesTwo: {
+    articlesTwo: {
         title: 'ARTICLE 2',
         head1 : 'This is Article 2 Deployed By Sathvik',
         content :`
                     <p>
                         Lorem ipsum dolor sit amet,consectetuer adipiscing elit. Aenean commodo ligula eget dur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. Maecenas tempus, tellus eget condimentum rhoncus, sem quam semper libero, sit amet adipiscing sem neque sed ipsum. Nam quam nunc, blandit vel, luctus pulvinar, hendrerit id, lorem. Maecenas nec odio et ante tincidunt tempus. Donec vitae sapien ut libero venenatis faucibus. Nullam quis ante. Etiam sit amet orci eget eros faucibus tincidunt. Duis leo. Sed fringilla mauris sit amet nibh. Donec sodales sagittis magna. Sed consequat, leo eget bibendum sodales, augue velit cursus nunc,
                     </p>`
-        };
+        },
+    articlesThree: {
+        title: 'ARTICLE 3',
+        head1 : 'This is Article 3 Deployed By Sathvik',
+        content :`
+                    <p>
+                        Lorem ipsum dolor sit amet,consectetuer adipiscing elit. Aenean commodo ligula eget dur ullamcorper ultricies nisi. Nam eget dui. Etiam rhoncus. 
+                    </p>`
+    }
 };
     
 function createTemplate (data){
