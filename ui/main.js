b5041de8a024fe0bfc4175f54120602b98b3cdb9
sathvikrijo <30945817+sathvikrijo@users.noreply.github.com>
2017-08-21 19:16:52 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -3,3 +3,9 @@ console.log('Loaded!');
 var element = document.getElementById("main-txt");
 
 element.innerHTML = "Hello World!!!";
+
+var img = document.getElementById("piggy");
+
+img.onclick = function(){
+    img.style.marginLeft = '100px';
+}
