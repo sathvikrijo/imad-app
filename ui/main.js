f162a2669a96d044e4312fc4ed5094ea158f7c17
sathvikrijo <30945817+sathvikrijo@users.noreply.github.com>
2017-08-21 19:24:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -9,10 +9,10 @@ var img = document.getElementById("piggy");
 var marginLeft = 0;
 
 function moveRight(){
-    marginLeft = marginLeft + 10;
+    marginLeft = marginLeft + 6;
     img.style.marginLeft = marginLeft+'px';
 }
 
 img.onclick = function(){
-    var interval = setInterval(moveRight, 100);
+    var interval = setInterval(moveRight, 50);
 }
