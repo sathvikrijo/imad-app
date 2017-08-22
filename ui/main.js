62e984883c052cc22286969337bb1146b4b2a9ad
sathvikrijo <30945817+sathvikrijo@users.noreply.github.com>
2017-08-22 20:26:33 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,18 +1,9 @@
-console.log('Loaded!');
+var button = document.getElementById('counter');
+var counter = 0 ;
 
-//var element = document.getElementById("main-txt");
-
-//element.innerHTML = "Hello World!!!";
-
-var img = document.getElementById("piggy");
-
-var marginLeft = 0;
-
-function moveRight(){
-    marginLeft = marginLeft + 6;
-    img.style.marginLeft = marginLeft+'px';
-}
-
-img.onclick = function(){
-    var interval = setInterval(moveRight, 50);
+button.onclick = function()
+{
+    counter = counter + 1;
+    var span = documentById('count');
+    span.innerHTML = counter.toString();
 }
