ba6b238ed3e12eb97d7f1f80ff16284aad0fc994
sathvikrijo <30945817+sathvikrijo@users.noreply.github.com>
2017-09-02 23:59:24 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,6 +1,25 @@
 var button = document.getElementById('counter');
 var counter = 0 ;
 
+button.onclick = function()
+{
+    var request = new XMLHttpRequest();
+    
+    request.onreadystatechange = function(){
+        if (request.readyState === XMLHttpRequest.DONE) {
+            if(request.status === 200){
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+    }
+            }
+        }
+    };
+    
+    request.open('GET', 'http://sathvikrijo.imad.hasura-app.io/counter', true);
+    request.send(null);
+};
+
 var nameInput = document.getElementById('name');
 var namey = nameInput.value; 
 var submit = document.getElementById('submit_btn');
@@ -26,6 +45,6 @@ button.onclick = function()
         }
     };
     
-    request.open('GET', 'http://sathvikrijo.imad.hasura-app.io/submit-name?name=' + name, true);
+    request.open('GET', 'http://sathvikrijo.imad.hasura-app.io/submit-name?name=' + namey, true);
     request.send(null);
 };
